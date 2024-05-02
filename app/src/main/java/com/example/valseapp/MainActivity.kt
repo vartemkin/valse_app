338dac22874a79f33ce0374a401b694ae3ed2104
Vladislav Artemkin <v.artemkin@ntechlab.com>
2024-05-02 14:17:41 +0300

    init

---
+++ app/src/main/java/com/example/valseapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.valseapp
+
+import android.os.Bundle
+import android.util.Log
+import android.webkit.ConsoleMessage
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.example.valseapp.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val webView = binding.webView
+        webView.setWebViewClient(WebViewClient())
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
+                Log.d("WebView", consoleMessage.message())
+                return true
+            }
+            override fun onPermissionRequest(request: PermissionRequest?) {
+                val resources = request?.resources
+                resources?.forEach { resource ->
+                    if (PermissionRequest.RESOURCE_PROTECTED_MEDIA_ID == resource) {
+                        request.grant(resources)
+                        return
+                    }
+                }
+                super.onPermissionRequest(request)
+            }
+        }
+
+        val webSettings: WebSettings = webView.getSettings()
+        webSettings.javaScriptEnabled = true
+        webSettings.builtInZoomControls = true
+
+        webView.loadUrl("https://google.ru/")
+    }
+}
